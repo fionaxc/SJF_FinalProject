f48c7c59a6223f9f23cdd0d4c364eb63e1632d26
Joyce Liao <jycqliao@gmail.com>
2019-01-10 11:01:27 -0500

    start menu

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "pages.h"
+
+int main() {
+    print_menu();
+
+    // get user selection
+    char buffer;
+    buffer = fgetc(stdin);
+
+    
+
+    return 0;
+}
---
+++ pages.c
@@ -0,0 +1,5 @@
+#include "pages.h"
+
+void print_menu() {
+    printf("Typing Test\n\nPress the corresponding number key to select menu items\n\n1) Start New Game\n2) View Leaderboard\n3) Exit\n");
+}
