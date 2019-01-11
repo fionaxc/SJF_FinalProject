6c320dd75407747b3a1540334fe6cd93a0d6df62
Joyce Liao <jycqliao@gmail.com>
2019-01-11 11:01:59 -0500

    menu

--- main.c
+++ main.c
@@ -1,13 +1,22 @@
 #include "pages.h"
 
 int main() {
-    print_menu();
 
-    // get user selection
-    char buffer;
-    buffer = fgetc(stdin);
+    while (1) {
+        print_menu();
 
-    
+        // send all keystrokes directly to stdin (read only one char)
+        system("/bin/stty raw");
+        // get user selection
+        char buffer;
+        buffer = fgetc(stdin);
+        // change to normal terminal setting
+        system ("/bin/stty cooked");
+
+        if (buffer == '3') { //exit is selected
+            exit(1);
+        }
+    } // end while
 
     return 0;
 }
