9ac05fac0278628a843ae1e4a19070699ff21a98
Fiona Cai <fionacai@Fionas-MacBook-Air.local>
2019-01-16 10:39:28 -0500

    working on game function

--- typing.c
+++ typing.c
@@ -1,10 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <string.h>
-#include <stdlib.h>
-#include "typing.h"
+#include <unistd.h>
+#include <math.h>
 
-void beginSession()
-{
+//Get the words from chosen story
+//input: file name, easy.txt, medium.txt, or hard.txt
+char ** getWords(char * story){
+  File * f = fopen(story, "r");
+  if(f == 0){
+    printf("Please input a valid story\n");
+    exit(1);
+  }
+  char input[512];
+  char ** dict = malloc(2048 * sizeof(char *));
+  while (fgets(input, sizeof(input), f)){
+    strcpy(dict[i], input);
+  }
+  fclose(f);
+  return dict;
+}
 
+
+
+void startGame(char ** dict){
+  char * intro="THE TYPING GAME BY FIONA, SOOJIN, AND JOYCE \n Choose your level of difficulty:";
+  char * s_easy="Easy: Press 1";
+  char * s_medium="Medium: Press 2";
+  char * s_hard="Hard: Press 3";
+  char * s_default="You did not press a valid key; Default level is Medium";
+
+  printf("%s\n %s\n %s\n %s\n", intro, s_easy, s_medium, s_hard);
+  if(dict == 0){
+    printf("Please input a valid dictionary\n");
+    return;
+  }
+  srand(time(0));
+
+  time_t start = time(0);
+  time_t current = 0;
+
+  char word[100];
+  char input[100];
+  int score = 0;
+
+  while (current < 60){ //currently easy level, CHANGE time later according to level
+    randomWord(dict, word);
+    printf("[%s]\n---------->\n", word);
+    scanf("%s", input); //get the user's input word
+
+  }
+  
 }
--- typing.h
+++ typing.h
@@ -1,23 +1,21 @@
-#define NLINES 5
-#define WIDTH 60
-#define SCRWIDTH 80
-#define SCRHEIGHT 24
-#define PAGE WIDTH*NLINES
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
 typedef struct
 {
 	char name[N];
-}user;
+} user;
 
 
 typedef struct
 {
 	 user u;
-   lesson lsn;
-   unsigned int wpm;
-   float accuracy;
-}session;
+   lesson l;
+   unsigned int wpm; //words per minute
+   float accuracy; //words
+} session;
 
-void beginSession(void);
-void displayResults(void);
+char ** getWords(char * story);
+void startGame(char ** dict);
